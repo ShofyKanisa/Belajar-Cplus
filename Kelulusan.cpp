4199bb05ed9fa615043c4456bc0d0c96593ef60d
ShofyKanisa <111962270+ShofyKanisa@users.noreply.github.com>
2022-11-23 17:42:10 +0700

    Add files via upload

---
+++ Kelulusan.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int nilai;
+  cout << "Masukan Nilai "; cin >> nilai;
+  if (nilai > 78){
+     cout << "Lulus";
+  }else if (nilai == 78){
+     cout << "KKM";
+  }else{
+     cout << "Tidak Lulus";
+       }
+       }
---
+++ Rumus.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Aplikasi Rumus"<<endl;
+    cout << "Masukan Angka Pertama= ";
+    int angka1;
+    cin >> angka1;
+    
+    cout << "Masukan Angka Kedua= ";
+    int angka2;
+    cin >> angka2;
+    
+    cout << "Masukan Angka Ketiga= ";
+    int angka3;
+    cin >> angka3;
+    
+    int hasil;
+        hasil = (angka1+angka2)/angka3;
+    cout << "Hasil Rumus= "<<hasil;
+}
