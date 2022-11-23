ddf670f001b065c3ee94f023f8d137c551467feb
ShofyKanisa <111962270+ShofyKanisa@users.noreply.github.com>
2022-11-23 16:58:55 +0700

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Name: Shofy Karimatunnisa" << endl;
+    cout << "Class: X PPLG 1" << endl;
+    cout << "==========" << endl;
+    cout << "angka1= 10" << endl;
+    cout << "angka2= 5" << endl;
+    cout << "hasil: " << 10+5 << endl;
+return 0;
+}
