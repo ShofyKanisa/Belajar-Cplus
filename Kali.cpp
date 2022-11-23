3afcd78c98cbafb86ee854257c7edb762644f6f6
ShofyKanisa <111962270+ShofyKanisa@users.noreply.github.com>
2022-11-23 17:19:34 +0700

    Add files via upload

---
+++ Kali.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Aplikasi Perkalian"<<endl;
+    cout << "Masukan Angka Pertama= ";
+    int angka1;
+    cin >> angka1;
+    
+    cout << "Masukan Angka Kedua= "<<endl;
+    int angka2;
+    cin >> angka2;
+    
+    int hasil;
+        hasil=angka1*angka2;
+    cout << "Hasil Perkalian= "<<hasil;
+}
